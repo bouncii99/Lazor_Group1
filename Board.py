c3e5e8944bc6fa073dc5247d531ec2f92281a917
Isaiah Chen <isaiahchen28@gmail.com>
2019-03-30 15:04:29 -0400

    Update Board.py
    Boards can now be read and parsed through

--- Board.py
+++ Board.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Board:
     """
     Board class represents the Lazors game board.
@@ -46,11 +49,41 @@ class Board:
 
 def read_bff(filename):
     """ Reads through the .bff file """
-    with open(filename) as fp:
-        for cnt, line in enumerate(fp):
-            print("Line {}: {}".format(cnt, line))
+    grid = []
+    reflect_blocks = []
+    opaque_blocks = []
+    refract_blocks = []
+    lasers = []
+    points = []
+    in_grid = False
+    raw_lines = open(filename, 'r')
+    for index, line in enumerate(raw_lines):
+        if "GRID STOP" in line:
+            in_grid = False
+        if in_grid:
+            line = line.replace(" ", "")
+            grid.append(list(line.strip("\n")))
+        if "GRID START" in line:
+            in_grid = True
+        if line[0] == "A":
+            reflect_blocks = [int(s) for s in line if s.isdigit()]
+        elif line[0] == "B":
+            opaque_blocks = [int(s) for s in line if s.isdigit()]
+        elif line[0] == "C":
+            refract_blocks = [int(s) for s in line if s.isdigit()]
+        if line[0] == "L":
+            lasers.append([int(s) for s in re.findall(r'-?\d', line)])
+        if line[0] == "P":
+            points.append([int(s) for s in line if s.isdigit()])
+    return grid, reflect_blocks, opaque_blocks, refract_blocks, lasers, points
 
 
 if __name__ == "__main__":
-    fptr = "dark_1.bff"
-    read_bff(fptr)
+    # Input file name
+    fptr = "yarn_5.bff"
+    # Read and parse through board file
+    g, rflb, ob, rfrb, l, p = read_bff(fptr)
+    # Make an instance of the board object and save it in a variable
+    board_row = len(g)
+    board_col = len(g[0])
+    board = Board(board_row, board_col)
