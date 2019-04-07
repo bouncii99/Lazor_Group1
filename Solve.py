afb53216394f7bd6bb42ca0bc6f850149ea8dfd4
Isaiah Chen <isaiahchen28@gmail.com>
2019-04-07 01:14:16 -0400

    Update Solve.py

--- Solve.py
+++ Solve.py
@@ -5,11 +5,15 @@ import Lazor
 
 
 def solve(board):
-    # From the blocks that are available, place them in random spots on the board
-    
-    while(positions != Filled):
-    	move block
-    	Refresh board
+    # From the blocks that are available, place them in random spots on the
+    # board
+	# INPUT FUNCTION/CODE TO PLACE BLOCKS HERE
+    # Initialize whatever is necessary to initialize
+    # while(all_positions != Filled):
+    	# move blocks to a new random positions
+    	# Refresh board
+	"""
+	pass
 
 if __name__ == "__main__":
-	solve()
+	print(Lazor.read_bff("mad_1.bff"))
