8c88553d8703aba7263e89591ed8acf44ed44f46
Isaiah Chen <isaiahchen28@gmail.com>
2019-04-07 00:56:03 -0400

    Create Solve.py

---
+++ Solve.py
@@ -0,0 +1,15 @@
+"""
+Solving algorithm
+"""
+import Lazor
+
+
+def solve(board):
+    # From the blocks that are available, place them in random spots on the board
+    
+    while(positions != Filled):
+    	move block
+    	Refresh board
+
+if __name__ == "__main__":
+	solve()
