7206d9e11eae2ae0a749caadb8b5131be5409f32
Isaiah Chen <isaiahchen28@gmail.com>
2019-04-14 19:20:28 -0400

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -1,7 +1,7 @@
 """
 Function for calculating laser points given a certain grid
 """
-
+import lazor
 
 def check_position(x, xmax, y, ymax):
     return x >= 0 and x <= xmax and y >= 0 and y <= ymax
@@ -21,20 +21,16 @@ def calc_center(p1, p2):
     else:
         return (nx, y)
 
-
-def map_to_block_grid(x, y):
-    nx = (x - 1) / 2
-    ny = (y - 1) / 2
+def map_to_block_grid(pos):
+    nx = (pos[0] - 1) / 2
+    ny = (pos[1] - 1) / 2
     return (nx, ny)
-
-
-def transmit():
-    pass
 
 def reflect():
     # At the point where the laser hits the block:
     # If x is even, flip the sign of y
     # If x is odd, flip the sign of x
+    # Create new laser object 
     pass
 
 def calculate_laser(grid, laser, transmit, reflect):
@@ -48,6 +44,8 @@ def calculate_laser(grid, laser, transmit, reflect):
     while True:
         # Check if the current point is in the grid
         if check_position(cx, xmax, cy, ymax):
+            pos = (cx, cy)
+            laser_points.append(pos)
             # Calculate the possible next point
             nx, ny = next_position(cx, cy, vx, vy)
             if check_position(nx, xmax, ny, ymax):
@@ -57,16 +55,18 @@ def calculate_laser(grid, laser, transmit, reflect):
                 # Determine if the block transmits and/or reflects the laser
                 does_transmit = transmit[block_position[0]][block_position[1]]
                 does_reflect = reflect[block_position[0]][block_position[1]]
-                if not does_transmit:
-
-                pos = (cx, cy)
-                laser_points.append(pos)
+                # Need to make conditional statements for all 4 cases and handle appropriately
+                if does_transmit:
+                    cx = nx
+                    cy = ny
+                if does_reflect:
+                    xtemp2, ytemp2 = reflect(cx, cy)
+                if not does_transmit and if not does_reflect 
+                raise Exception
             else:
                 pass
         else:
             break
-        cx = nx
-        cy = ny
     return laser_points
 
 
